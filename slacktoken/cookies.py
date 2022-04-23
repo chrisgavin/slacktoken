6d0aa18859655209daf99729b3451d37db3e4480
Chris Gavin <chris@chrisgavin.me>
2022-04-23 16:42:05 +0100

    Fix handling for encrypted cookies on Linux.

---
+++ slacktoken/cookies.py
@@ -0,0 +1,81 @@
+import pathlib
+import sqlite3
+import sys
+import typing
+
+import cryptography.hazmat.primitives.ciphers
+import cryptography.hazmat.primitives.ciphers.algorithms
+import cryptography.hazmat.primitives.ciphers.modes
+import cryptography.hazmat.primitives.hashes
+import cryptography.hazmat.primitives.kdf.pbkdf2
+
+import slacktoken.exceptions
+
+def _get_encryption_password() -> bytes:
+	if sys.platform == "linux":
+		import gi
+		gi.require_version("Secret", "1")
+		import gi.repository.Secret
+
+		service = gi.repository.Secret.Service.get_sync(gi.repository.Secret.ServiceFlags.LOAD_COLLECTIONS)
+		keyring_collections = service.get_collections()
+		unlocked_keyrings = service.unlock_sync(keyring_collections).unlocked
+
+		for keyring in unlocked_keyrings:
+			print("Searching keyring:", keyring.get_label())
+			for item in keyring.get_items():
+				print("Searching item:", item.get_label())
+				if item.get_label() == "Chromium Safe Storage":
+					item.load_secret_sync()
+					secret_text = item.get_secret().get_text()
+					assert isinstance(secret_text, str)
+					return secret_text.encode("utf-8")
+
+		raise slacktoken.exceptions.InternalException("Couldn't find encryption keyring item.")
+	else:
+		raise slacktoken.exceptions.InternalException(f"Cookie decryption not implemented on {sys.platform}.")
+
+def _decrypt(encrypted_value:bytes) -> str:
+	encrypted_value = encrypted_value[3:] # Encrypted cookies start with a version number. e.g. "v11"
+	password = _get_encryption_password()
+	
+	length = 16
+	salt = b"saltysalt"
+	iterations = 1
+	kdf = cryptography.hazmat.primitives.kdf.pbkdf2.PBKDF2HMAC(
+		algorithm=cryptography.hazmat.primitives.hashes.SHA1(),
+		length=length,
+		iterations=iterations,
+		salt=salt,
+	)
+
+	key = kdf.derive(password)
+	iv = b" " * length
+	cipher = cryptography.hazmat.primitives.ciphers.Cipher(
+		algorithm=cryptography.hazmat.primitives.ciphers.algorithms.AES(key),
+		mode=cryptography.hazmat.primitives.ciphers.modes.CBC(iv),
+	)
+
+	decryptor = cipher.decryptor()
+	decrypted_value = decryptor.update(encrypted_value) + decryptor.finalize()
+
+	length_byte = decrypted_value[-1]
+	cleaned_value = decrypted_value[:-length_byte]
+
+	assert isinstance(cleaned_value, bytes)
+	return cleaned_value.decode("utf-8")
+	
+def get_cookies(configuration_directory:pathlib.Path) -> typing.Dict[str, str]:
+	cookie_database_path = configuration_directory / "Cookies"
+	if not cookie_database_path.is_file():
+		raise slacktoken.exceptions.MissingSlackData("cookie database")
+	cookie_database = sqlite3.connect(str(cookie_database_path))
+	cookie_cursor = cookie_database.cursor()
+	cookie_cursor.execute("SELECT value, encrypted_value FROM cookies WHERE host_key=\".slack.com\" AND name=\"d\"")
+	cookie = cookie_cursor.fetchone()
+	if cookie is None:
+		raise slacktoken.exceptions.MissingSlackData("login cookie")
+	value, encrypted_value = cookie
+	if value == "" and encrypted_value != "":
+		value = _decrypt(encrypted_value)
+	return {"d": value}
--- slacktoken/token.py
+++ slacktoken/token.py
@@ -3,11 +3,11 @@ import os
 import pathlib
 import platform
 import re
-import sqlite3
 import typing
 
 import requests
 
+import slacktoken.cookies
 import slacktoken.exceptions
 
 _API_TOKEN_MATCHER = re.compile("\"api_token\":\"([^\"]+)\"")
@@ -27,18 +27,6 @@ def _get_slack_configuration_directory() -> pathlib.Path:
 		config_directory = pathlib.Path.home() / ".config"
 	return config_directory / "Slack"
 
-def _get_slack_cookies() -> typing.Dict[str, str]:
-	cookie_database_path = _get_slack_configuration_directory() / "Cookies"
-	if not cookie_database_path.is_file():
-		raise slacktoken.exceptions.MissingSlackData("cookie database")
-	cookie_database = sqlite3.connect(str(cookie_database_path))
-	cookie_cursor = cookie_database.cursor()
-	cookie_cursor.execute("SELECT value FROM cookies WHERE host_key=\".slack.com\" AND name=\"d\"")
-	cookie = cookie_cursor.fetchone()
-	if cookie is None:
-		raise slacktoken.exceptions.MissingSlackData("login cookie")
-	return {"d": cookie[0]}
-
 def _get_slack_workspaces() -> typing.List[str]:
 	root_state_path = _get_slack_configuration_directory() / "storage" / "root-state.json"
 	if not root_state_path.is_file():
@@ -48,7 +36,8 @@ def _get_slack_workspaces() -> typing.List[str]:
 	return [workspace["domain"] for workspace in workspaces.values()]
 
 def get(workspace:typing.Optional[str]) -> SlackAuthenticationInformation:
-	cookies = _get_slack_cookies()
+	configuration_directory = _get_slack_configuration_directory()
+	cookies = slacktoken.cookies.get_cookies(configuration_directory)
 	workspaces = _get_slack_workspaces()
 	if not workspaces:
 		raise slacktoken.exceptions.NotSignedInException()
@@ -60,7 +49,7 @@ def get(workspace:typing.Optional[str]) -> SlackAuthenticationInformation:
 	else:
 		if workspace not in workspaces:
 			raise slacktoken.exceptions.NotSignedInToWorkspaceException(workspace)
-	response = requests.get(f"https://{workspace}.slack.com/", cookies=cookies)
+	response = requests.get(f"https://{workspace}.slack.com/ssb/redirect", cookies=cookies)
 	matcher = _API_TOKEN_MATCHER.search(response.text)
 	if matcher is None:
 		raise slacktoken.exceptions.InternalException("Could not extract Slack token from webpage. Maybe the Slack webpage has changed.")
