0dd92ceb79b3f4f654f1bc70746ae710d83b7a56
Chris Gavin <chris@chrisgavin.me>
2022-05-07 12:48:32 +0100

    Add some optional debug logging.

--- slacktoken/__main__.py
+++ slacktoken/__main__.py
@@ -1,10 +1,14 @@
 import argparse
+import logging
 
 import slacktoken.commands.get
 import slacktoken.commands.version
 
+_LOGGER = logging.getLogger("slacktoken")
+
 def _parse_arguments() -> argparse.Namespace:
 	argument_parser = argparse.ArgumentParser(description="A library for retrieving a Slack user token from an authenticated Slack application.")
+	argument_parser.add_argument("--verbose", action="store_true", help="Increases the log level.")
 	subparsers = argument_parser.add_subparsers(help="The command to run.", dest="command")
 	subparsers.required = True
 	subparsers.add_parser("version", help="Show the version of the application.")
@@ -14,6 +18,8 @@ def _parse_arguments() -> argparse.Namespace:
 
 def main() -> None:
 	arguments = _parse_arguments()
+	logging.basicConfig()
+	_LOGGER.setLevel(logging.DEBUG if arguments.verbose else logging.INFO)
 	if arguments.command == "version":
 		slacktoken.commands.version.command(arguments)
 	if arguments.command == "get":
