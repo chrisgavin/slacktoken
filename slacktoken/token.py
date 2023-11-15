ea910b963a52826389cd7d291b470e0a7f36a03a
Chris Gavin <chris@chrisgavin.me>
2023-11-15 09:49:20 +0000

    Update token fetching for new page design.

--- slacktoken/token.py
+++ slacktoken/token.py
@@ -6,38 +6,31 @@ import requests
 
 import slacktoken.cookies
 import slacktoken.exceptions
-import slacktoken.slack_configuration
 
-_API_TOKEN_MATCHER = re.compile("\"api_token\":\"([^\"]+)\"")
+_AUTHENTICATION_JSON_MATCHER = re.compile("JSON\\.stringify\\((.+)\\);")
 
 class SlackAuthenticationInformation():
 	def __init__(self, token:str, cookies:typing.Dict[str, str]):
 		self.token = token
 		self.cookies = cookies
 
-def _get_slack_workspaces() -> typing.List[str]:
-	root_state_path = slacktoken.slack_configuration.get_slack_configuration_directory() / "storage" / "root-state.json"
-	if not root_state_path.is_file():
-		raise slacktoken.exceptions.MissingSlackData("workspaces file")
-	with root_state_path.open() as root_state_file:
-		workspaces = json.load(root_state_file)["workspaces"]
-	return [workspace["domain"] for workspace in workspaces.values()]
-
 def get(workspace:typing.Optional[str]) -> SlackAuthenticationInformation:
 	cookies = slacktoken.cookies.get_cookies()
-	workspaces = _get_slack_workspaces()
-	if not workspaces:
-		raise slacktoken.exceptions.NotSignedInException()
-	if workspace is None:
-		if len(workspaces) == 1:
-			workspace = workspaces[0]
-		else:
-			raise slacktoken.exceptions.AmbiguousWorkspaceException(workspaces)
-	else:
-		if workspace not in workspaces:
-			raise slacktoken.exceptions.NotSignedInToWorkspaceException(workspace)
-	response = requests.get(f"https://{workspace}.slack.com/ssb/redirect", cookies=cookies)
-	matcher = _API_TOKEN_MATCHER.search(response.text)
+
+	response = requests.get("https://app.slack.com/auth", params={"app": "client"}, cookies=cookies)
+	matcher = _AUTHENTICATION_JSON_MATCHER.search(response.text)
 	if matcher is None:
 		raise slacktoken.exceptions.InternalException("Could not extract Slack token from webpage. Maybe the Slack webpage has changed.")
-	return SlackAuthenticationInformation(matcher.group(1), cookies)
+	authentication_information = json.loads(matcher.group(1))
+
+	if workspace is None:
+		if len(authentication_information["teams"]) == 1:
+			workspace = list(authentication_information["teams"].values())[0]["domain"]
+		else:
+			raise slacktoken.exceptions.AmbiguousWorkspaceException([team["domain"] for team in authentication_information["teams"]])
+	
+	workspace_informations = [team for team in authentication_information["teams"].values() if team["domain"] == workspace]
+	if len(workspace_informations) == 0:
+		raise slacktoken.exceptions.NotSignedInToWorkspaceException(workspace)
+
+	return SlackAuthenticationInformation(workspace_informations[0]["token"], cookies)
