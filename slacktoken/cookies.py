67aabf713294f7f488e03e9d49c3908739ecc009
Chris Gavin <chris@chrisgavin.me>
2024-11-07 03:29:07 +0000

    Fix cookie decryption with the latest Slack client.

--- slacktoken/cookies.py
+++ slacktoken/cookies.py
@@ -17,15 +17,24 @@ def get_cookies() -> typing.Dict[str, str]:
 		raise slacktoken.exceptions.MissingSlackData("cookie database")
 	_LOGGER.debug("Reading cookies from %s.", cookie_database_path)
 	cookie_database = sqlite3.connect(str(cookie_database_path))
+	cookie_database.text_factory = bytes
+	version_cursor = cookie_database.cursor()
+	version_cursor.execute("SELECT value FROM meta WHERE key=\"version\"")
+	version = int(version_cursor.fetchone()[0])
 	cookie_cursor = cookie_database.cursor()
 	cookie_cursor.execute("SELECT value, encrypted_value FROM cookies WHERE host_key=\".slack.com\" AND name=\"d\"")
 	cookie = cookie_cursor.fetchone()
 	if cookie is None:
 		raise slacktoken.exceptions.MissingSlackData("login cookie")
 	value, encrypted_value = cookie
-	if value == "" and encrypted_value != "":
+	value = value.decode("utf-8")
+	if len(value) == 0 and len(encrypted_value) > 0:
 		_LOGGER.debug("Cookie is encrypted. Will attempt to decrypt...")
-		value = slacktoken.decryptor.decrypt(encrypted_value)
+		decrypted_value = slacktoken.decryptor.decrypt(encrypted_value)
+		if version >= 24:
+			# Remove the domain hash from the encrypted value.
+			decrypted_value = decrypted_value[32:]
+		value = decrypted_value.decode("utf-8")
 	else:
 		_LOGGER.debug("Cookie is not encrypted.")
 	return {"d": value}
