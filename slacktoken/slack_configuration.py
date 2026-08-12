d72dbf91b238c93f6a665315c31b85b33ead4fe9
Chris Gavin <chris@chrisgavin.me>
2026-08-12 11:11:43 +0100

    Update path to Slack on Windows.

--- slacktoken/slack_configuration.py
+++ slacktoken/slack_configuration.py
@@ -15,7 +15,14 @@ def get_slack_configuration_directory() -> pathlib.Path:
 				return possible_config_directory
 		return possible_config_directories[0]
 	elif sys.platform == "win32":
-		return pathlib.Path(os.environ["APPDATA"]) / "Slack"
+		possible_config_directories = [
+			pathlib.Path(os.environ["APPDATA"]) / "Slack",
+			pathlib.Path(os.environ["LOCALAPPDATA"]) / "Packages" / "com.tinyspeck.slackdesktop_8yrtsj140pw4g" / "LocalCache" / "Roaming" / "Slack",
+		]
+		for possible_config_directory in possible_config_directories:
+			if possible_config_directory.is_dir():
+				return possible_config_directory
+		return possible_config_directories[0]
 	elif _XDG_CONFIG_DIR_VARIABLE in os.environ:
 		return pathlib.Path(_XDG_CONFIG_DIR_VARIABLE) / "Slack"
 	else:
